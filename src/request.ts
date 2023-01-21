ea401b81d33f743339c9c3de2374f7fa821104ef
Christoffer Öhman <totalchriz@gmail.com>
2023-01-21 01:50:53 +0100

    relocated

--- src/request.ts
+++ src/request.ts
@@ -1,11 +1,69 @@
-import { HttpResponse, Publisher, RequestConfig } from './types';
 import { jsonResponse } from './response';
+import { HttpRequest, InitConfig, Publisher, RequestConfig, RequestMethods } from './types';
 
-function requestInit(publisher: Publisher) {
-	return async <Data = unknown>(url: string, config: RequestConfig) => {
-		const response = await fetch(url, publisher.request(config));
+function get(publisher: Publisher) {
+	return async <Data = unknown>(url: string, config: RequestConfig = {}) => {
+		const response = await fetch(
+			url,
+			publisher.request({
+				method: RequestMethods.GET,
+				...config,
+			})
+		);
+
 		return await jsonResponse<Data>(publisher.response(response));
 	};
 }
 
-export { requestInit };
+function put(publisher: Publisher) {
+	return async <Data = unknown>(url: string, config: RequestConfig = {}) => {
+		const response = await fetch(
+			url,
+			publisher.request({
+				method: RequestMethods.GET,
+				...config,
+			})
+		);
+
+		return await jsonResponse<Data>(publisher.response(response));
+	};
+}
+
+function post(publisher: Publisher) {
+	return async <Data = unknown>(url: string, config: RequestConfig = {}) => {
+		const response = await fetch(
+			url,
+			publisher.request({
+				method: RequestMethods.GET,
+				...config,
+			})
+		);
+
+		return await jsonResponse<Data>(publisher.response(response));
+	};
+}
+
+function _delete(publisher: Publisher) {
+	return async <Data = unknown>(url: string, config: RequestConfig = {}) => {
+		const response = await fetch(
+			url,
+			publisher.request({
+				method: RequestMethods.GET,
+				...config,
+			})
+		);
+
+		return await jsonResponse<Data>(publisher.response(response));
+	};
+}
+
+function request({ baseUrl, ...requestConfig }: InitConfig, verb: HttpRequest) {
+	return async <Data = unknown>(path: string, config: RequestConfig = {}) => {
+		return await verb<Data>(baseUrl + path, {
+            ...requestConfig,
+			...config,
+		});
+	};
+}
+
+export { request, get, put, post, _delete };
